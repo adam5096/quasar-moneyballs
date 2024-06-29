2140697bf595b978889aad84b7dd2d436bd95a66
adam5096 <adamchou5096@gmail.com>
2024-06-29 15:24:12 +0800

    -feat: Add Entry 新增 新增事件功能
    調整項目
    1. src/pages/PageEntries.vue
    @ 1. 新增 輸入框取得焦點
    @ 2. 新增 數據初始態
    @ 3. 新增 數據操作態
    @ 4. 新增 清空前次輸入框遺留內容
    @ 5. 新增 點擊按鈕 q-btn icon="add"後續邏輯
    @ 6. 修改輸入區塊 div 改為 q-form 並新增@submit事件
    @ 7. 輸入區塊中，新增各輸入功能註解

--- src/pages/PageEntries.vue
+++ src/pages/PageEntries.vue
@@ -22,17 +22,21 @@
                     {{ useCurrencify(balance) }}
                 </div>
             </div>
-            <div class="row q-px-sm q-pb-sm q-col-gutter-sm bg-primary">
+            <q-form @submit="addEntry" class="row q-px-sm q-pb-sm q-col-gutter-sm bg-primary">
+                <!-- 名稱輸入 -->
                 <div class="col">
-                    <q-input outlined placeholder="Name" bg-color="white" dense />
+                    <q-input ref="nameRef" v-model="addEntryForm.name" outlined placeholder="Name" bg-color="white" dense />
                 </div>
+                <!-- 金額輸入 -->
                 <div class="col">
-                    <q-input input-class="text-right" placeholder="Amount" bg-color="white" type="number" step="0.01" outlined dense />
+                    <!-- v-model.number 將數據存本地元件保存前先轉換成number格式 -->
+                    <q-input v-model.number="addEntryForm.amount" input-class="text-right" placeholder="Amount" bg-color="white" type="number" step="0.01" outlined dense />
                 </div>
+                <!-- 送出表單 -->
                 <div class="col col-auto">
-                    <q-btn round color="primary" icon="add" />
+                    <q-btn type="submit" round color="primary" icon="add" />
                 </div>
-            </div>
+            </q-form>
         </q-footer>
     </q-page>
 </template>
@@ -40,10 +44,12 @@
 <script setup>
 // import
 // 分別匯入vue相關函數
-import { ref, computed } from 'vue'
-// 匯入封裝的貨幣呈現 格式化函數
+import { ref, reactive, computed } from 'vue'
+// 分別匯入quasar提供的uid函數
+import { uid } from 'quasar'
+// 匯入客製封裝的貨幣呈現 格式化函數
 import { useCurrencify } from "src/use/useCurrency.js";
-// 匯入封裝的金流顯色函數
+// 匯入封裝的金流顯色函數 income綠色 expense紅色
 import { useAmountColorClass } from "src/use/useAmountColorClass.js";
 defineOptions({
     name: 'IndexPage'
@@ -73,7 +79,6 @@ const entries = ref([{
         id: 'id4',
         name: 'Unknown',
         amount: 0
-
     }
 ])
 
@@ -84,4 +89,60 @@ const balance = computed(() => {
         return accumulator + amount
     }, 0)
 })
+
+// 建立輸入框實例 placeholder="Name"
+// 後續實作輸入框取得焦點
+const nameRef = ref(null)
+
+// 建立一個"清空數據時"，要恢復到此的數據狀態，數據初始態
+const addEntryFormDefault = {
+    name: '',
+    amount: null
+}
+
+// add entry
+// 1. 保存 即將新增的資料 input placeholder為Name Amount的兩個輸入框
+// 2. 表單上的雙項數據綁定互動對象都以此為主要窗口
+// 3. 與 數據初始態 分隔開，確保數據流向的固定
+const addEntryForm = reactive({
+    ...addEntryFormDefault
+})
+
+// 清空前次輸入框遺留內容
+const addEntryFormReset = () => {
+    // 寫法一
+    // console.log('reset');
+    // addEntryForm.name = ''
+    // number型別數據清空建議使用null
+    // addEntryForm.amount = null
+
+    // 寫法二
+    Object.assign(addEntryForm, addEntryFormDefault)
+
+    // 輸入框再次獲得焦點 節約瑣碎的滑鼠操作
+    nameRef.value.focus()
+}
+
+// 點擊按鈕 q-btn icon="add" 後的邏輯
+// @submit 監聽事件綁在q-form元素的開始標籤裡面，而非q-btn icon="add"上哦
+const addEntry = () => {
+    // console.log('addEntry');
+
+    // 寫法一
+    // const newEntry = {
+    //   id:uid(),
+    //   name:addEntryForm.name,
+    //   amount:addEntryForm.amount
+    // }
+
+    // 寫法二
+    // 1. 語法 Object.assign(既有陣列,新陣列,合併陣列中要新增的屬性名、值)
+    // 2. 第3參數是 既有陣列與新陣列中都沒有的全新屬性與值，如果想要新增這樣性質的屬性可以往第3參數位置書寫
+    const newEntry = Object.assign({}, addEntryForm, { id: uid() })
+    // console.log('newEntry',newEntry);
+    // 將使用者輸入內容推送到列尾部並重新渲染至頁面上
+    entries.value.push(newEntry)
+    // 清空前次輸入框遺留內容
+    addEntryFormReset()
+}
 </script>
